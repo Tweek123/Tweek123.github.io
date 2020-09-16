4ea43ca83c7385bfc21411b485d4c33a1d68e37c
Tweek123 <swapep@mail.com>
2020-09-16 23:00:52 +0300

    deploy

---
+++ precache-manifest.80b86622c24cd4f9b6d9fa9527647425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5f6743dc99a47952c7974fb4d9c390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57bcc2cf984bc5940198",
+    "url": "/static/css/main.053c8232.chunk.css"
+  },
+  {
+    "revision": "a3bd8b0391bfc93fb525",
+    "url": "/static/js/2.19c8f93a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.19c8f93a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57bcc2cf984bc5940198",
+    "url": "/static/js/main.aeea53be.chunk.js"
+  },
+  {
+    "revision": "d843c725febd61cf4753",
+    "url": "/static/js/runtime-main.26490a49.js"
+  }
+]);
